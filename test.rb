cdcf46fe562fcbac0a413714ff4482715e006bc2
shen-sat <shensat18@gmail.com>
2018-01-26 17:46:37 +0000

    added find table max method

--- test.rb
+++ test.rb
@@ -19,32 +19,51 @@ MySearch.new(xml, "xml").search_hash(xml, "Boetsch, Tim").result
 MySearch.new(xml, "xml").search_hash(xml, "Middleweights (185 lb, 84 kg)").result
 =end
 
+class Output
+  def initialize
+    document = File.open('./ufc.html')
+    content = document.read
+    @xml = Crack::XML.parse(content)
+  end
 
-
-
-
-
-def print_names(table_number)
-  document = File.open('./ufc.html')
-  content = document.read
-  xml = Crack::XML.parse(content)
-  my_range = (1..100)
-  begin
-    my_range.each do |replace|
-      n = replace
-      value = xml["html"]["body"]["div"][2]["div"][2]["div"][3]["div"]["table"][table_number]["tr"][n]["td"][2]["span"][0]
-      puts value
+  def find_table_max
+    my_range = (1..100)
+    table_number = 0
+    my_range.each do |number|
+      table_number = number
+      #NOTE TO SELF: THE REASON YOU COULDNT RAISE AN ERROR HERE IS BECAUSE IF table_number is nil, you don't call anything on it, and so it never raises and error, it simply returns nil
+      test_table = @xml["html"]["body"]["div"][2]["div"][2]["div"][3]["div"]["table"][table_number]
+      break if test_table.nil?   
     end
-  rescue 
-    puts "There is an error:"
-    if table_number < 6
-      table_number += 1
-      print_names(table_number)
+    table_max = table_number - 1
+    return table_max  
+  end
+
+  def print_names(table_number, table_max)
+    my_range = (1..100)
+    begin
+      my_range.each do |replace|
+        n = replace
+        value = @xml["html"]["body"]["div"][2]["div"][2]["div"][3]["div"]["table"][table_number]["tr"][n]["td"][2]["span"][0]
+        puts value
+      end
+    rescue => e
+      puts e
+      puts "Either table not formatted as expected, or END OF TABLE"
+      if table_number < table_max
+        table_number += 1
+        print_names(table_number, table_max)
+      end
     end
   end
+
 end
 
-print_names(4)
+output = Output.new
+output.print_names(1, output.find_table_max)
+
+#print_names(1,find_table_max)
+#find_table_max
 
 
 
